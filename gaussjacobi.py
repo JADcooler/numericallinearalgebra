a69268ca21b3b3fd3c4ecc0620bc956d6a59781c
jadejassajay <jadejassajay@gmail.com>
2021-06-19 20:44:48 +0530

    Update gaussjacobi.py

--- gaussjacobi.py
+++ gaussjacobi.py
@@ -4,6 +4,7 @@ a=[[5,-2,3],[-3,9,1],[2,-1,-7]]
 b=[-1,2,3]
 x=[0,0,0]
 xtemp=[0,0,0]
+round_till=3
 for g in range(10): # 10 iterations
     print("iteration ",g+1)
     x=xtemp.copy()
@@ -19,9 +20,9 @@ for g in range(10): # 10 iterations
                 print(-a[i][j],end='*')
                 print(x[j],end=' ')
         xtemp[i]=sum/a[i][i]
-        xtemp[i]=round(xtemp[i],4)
+        xtemp[i]=round(xtemp[i],round_till)
         print(")/",a[i][i],end='')
-        print(" = ",round(sum,4),'/',a[i][i])
+        print(" = ",round(sum,round_till),'/',a[i][i])
 
     for i in range(n):
         print("x",i+1," is ",xtemp[i])
